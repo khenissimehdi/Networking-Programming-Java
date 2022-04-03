640530fda6a844110b70a68c4dd36d10495396ee
khenissimehdi <khenissimehdi0123@gmail.com>
2022-04-03 08:51:02 +0200

    echoPlus finished

---
+++ src/tp6/exo2/ServerEchoPlus.java
@@ -0,0 +1,96 @@
+package tp6.exo1;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
+
+public class ServerEcho {
+    private static final Logger logger = Logger.getLogger(ServerEcho.class.getName());
+
+    private final DatagramChannel dc;
+    private final Selector selector;
+    private final int BUFFER_SIZE = 1024;
+    private final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+    private SocketAddress sender;
+    private int port;
+
+    public ServerEcho(int port) throws IOException {
+        this.port = port;
+        selector = Selector.open();
+        dc = DatagramChannel.open();
+        dc.bind(new InetSocketAddress(port));
+        dc.configureBlocking(false);
+
+        // This depends on the state of the server, its echo, so you have to wait a value
+        dc.register(selector, SelectionKey.OP_READ);
+    }
+
+    public void serve() throws IOException {
+        logger.info("ServerEcho started on port " + port);
+        while (!Thread.interrupted()) {
+            try {
+                selector.select(this::treatKey);
+            } catch (UncheckedIOException e) {
+                throw e.getCause();
+            }
+
+        }
+    }
+
+    private void treatKey(SelectionKey key) {
+        try {
+            if (key.isValid() && key.isWritable()) {
+                doWrite(key);
+            }
+            if (key.isValid() && key.isReadable()) {
+                doRead(key);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+    }
+
+    private void doRead(SelectionKey key) throws IOException {
+        sender = dc.receive(buffer);
+        if(sender == null) {
+            System.out.println("i got nth");
+        } else {
+            key.interestOps(SelectionKey.OP_WRITE);
+            buffer.flip(); // we have to put this here cause we are sure we got the value and that we are going to the onWrite and not coming back to the OnRead
+        }
+    }
+
+    private void doWrite(SelectionKey key) throws IOException {
+        if(sender != null) {
+            // No Flip here you dumb fuck
+            dc.send(buffer, sender);
+            if(buffer.remaining() != 0) {
+                System.out.println("nth was sent");
+            } else {
+                buffer.clear();
+                key.interestOps(SelectionKey.OP_READ);
+            }
+        }
+    }
+
+    public static void usage() {
+        System.out.println("Usage : ServerEcho port");
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            usage();
+            return;
+        }
+        new ServerEcho(Integer.parseInt(args[0])).serve();
+    }
+}
